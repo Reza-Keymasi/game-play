3396bb8c03b752c25ebcebbb62454b8229b79f67
Reza-Keymasi <rezakeymasi1996@gmail.com>
2023-04-01 21:09:26 +0430

    build layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { Grid } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
-
   return (
     <>
-      <Grid></Grid>
+      <Grid
+        templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      >
+        <GridItem bg="coral" area={"nav"}>
+          navbar
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="gold" area={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem bg="dodgerblue" area={"main"}>
+          Main
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
