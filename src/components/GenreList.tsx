8a16c335a1d9f4a4b4c7dfc1cb6fe3b2c5f513bb
Reza-Keymasi <rezakeymasi1996@gmail.com>
2023-04-19 14:16:56 +0430

    creating a generic data fetching hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import useGenre from '../hooks/useGenre'
 
 const GenreList = () => {
-  const {genres} = useGenre()
+  const {data} = useGenre()
   return (
-    <ul>{genres.map(genre => (<li key={genre.id}>{genre.name}</li>))}</ul>
+    <ul>{data.map(genre => (<li key={genre.id}>{genre.name}</li>))}</ul>
   )
 }
 
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,49 +1,18 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/client-api";
-import { CanceledError } from "axios";
-
+import useData from "./useData";
 
 export interface Platform {
-  id:number;
-  name:string;
-  slug:string
+  id: number;
+  name: string;
+  slug: string;
 }
 export interface Game {
   id: number;
   name: string;
-  background_image:string;
-  parent_platforms: {platform:Platform}[];
-  metacritic:number
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
-const useGames = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading,setIsLoading] = useState(false)
-  useEffect(() => {
-    const controller = new AbortController()
-    setIsLoading(true)
-    apiClient
-      .get<FetchGamesResponse>("/games", {signal:controller.signal})
-      .then((response) => {
-        setGames(response.data.results)
-        setIsLoading(false)
-      })
-      .catch((err) => {
-        if(err instanceof CanceledError) return;
-        setError(err.message)
-        setIsLoading(false)
-      });
-
-      return () => controller.abort()
-  }, []);
-
-  return {games, error, isLoading}
-};
+const useGames = () => useData<Game>("/games");
 
 export default useGames;
