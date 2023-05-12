b4bfc58cacd224c67793b6b152d2aab16f06c530
Reza-Keymasi <rezakeymasi1996@gmail.com>
2023-05-13 01:03:15 +0330

    Handle Errors

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,4 +1,5 @@
 import {createBrowserRouter} from "react-router-dom"
+import ErrorPage from "../pages/ErrorPage"
 import GameDatailsPage from "../pages/GameDatailsPage"
 import HomePage from "../pages/HomePage"
 import Layout from "../pages/Layout"
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
     {
         path: "/",
         element:<Layout />,
+        errorElement:<ErrorPage />,
         children:[
             {index:true,element:<HomePage />},
             {path:"games/:id",element:<GameDatailsPage />},
