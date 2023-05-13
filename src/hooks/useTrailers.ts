29cf549a87a3fb1b16666c778b057ff9522bfc6b
Reza-Keymasi <rezakeymasi1996@gmail.com>
2023-05-13 09:38:31 +0330

    Build Game Trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { Trailer } from "../entities/Trailer";
+import APIClient from "../services/client-api";
+
+
+
+const useTrailers = (gameId:number) => {
+    const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`)
+    return useQuery({
+        queryKey:["trailers", gameId],
+        queryFn: apiClient.getAll
+    })
+} 
+
+export default useTrailers
